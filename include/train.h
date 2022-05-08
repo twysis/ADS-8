c6965b7e79745feca24b7ca70fcc5d4dc522b0f6
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-05-08 14:45:21 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -9,8 +9,9 @@ class Train {
     Cage *next;
     Cage *prev;
   };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
+  int countOp, length, x; // счетчик шагов (число переходов из вагона в вагон)
+  Cage *first;
+  Cage* last;// точка входа в поезд (первый вагон)
  public:
   Train();
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
